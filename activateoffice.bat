0f590853e0aeaf7a9c381b75443a6e1574728450
Axel Clover <kashccotton@gmail.com>
2022-07-25 09:35:35 -0700

    Activate Office

---
+++ activateoffice.bat
@@ -0,0 +1,19 @@
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+@echo off
+title Activate Microsoft Office&cls&echo =====================================================================================&echo #Project: Activating Microsoft software products for FREE without additional software&echo =====================================================================================&echo.&echo #Supported products:&echo - Microsoft Office Standard 2019&echo - Microsoft Office Professional Plus 2019&echo.&echo.&(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")&(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&echo.&echo ============================================================================&echo Activating your Office...&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo ospp.vbs /setprt:1688 >nul&cscript //nologo ospp.vbs /unpkey:6MWKP >nul&set i=1&cscript //nologo ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP >nul||goto notsupported
+:skms
+if %i% GTR 10 goto busy
+if %i% EQU 1 set KMS=kms7.MSGuides.com
+if %i% EQU 2 set KMS=s8.uk.to
+if %i% EQU 3 set KMS=s9.us.to
+if %i% GTR 3 goto ato
+cscript //nologo ospp.vbs /sethst:%KMS% >nul
+:ato
+echo ============================================================================&echo.&echo.&cscript //nologo ospp.vbs /act | find /i "successful" && (echo.&echo ========================================================================================================================================================&choice /n /c YN /m "Would you like to visit my blog [Y,N]?" & if errorlevel 2 exit) || (echo The connection to my KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto skms)
+explorer "http://github.com/axel-clover"&goto halt
+:notsupported
+echo ============================================================================&echo.&echo Sorry, your version is not supported.&echo.&goto halt
+:busy
+echo ============================================================================&echo.&echo Sorry, the server is busy and can't respond to your request. Please try again.&echo.
+:halt
+pause >nul
